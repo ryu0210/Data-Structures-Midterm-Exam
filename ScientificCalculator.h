225da14d66252f09266dedafda79bfc92aafed80
ryu0210 <89006072+ryu0210@users.noreply.github.com>
2022-10-12 13:29:53 +0700

    Update ScientificCalculator.h

--- ScientificCalculator.h
+++ ScientificCalculator.h
@@ -6,11 +6,15 @@ class ScientificCalculator
 {
     public:
         ScientificCalculator();
-        virtual ~ScientificCalculator();
-
-    protected:
+        void Calculation(int choice);
+        void Continuing_Calculation(int choice);
+        void Alternate_Calculation(int choice);
+        void Alternate_Calculation_2(int choice);
+        void Save_History(void);
+        void Print_History(void);
 
     private:
+        int Option;
 };
 
 #endif // SCIENTIFICCALCULATOR_H
