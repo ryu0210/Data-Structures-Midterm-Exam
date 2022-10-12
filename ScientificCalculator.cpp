26b4dfa5c54d97c13c6986311bdb2988fef84f88
ryu0210 <89006072+ryu0210@users.noreply.github.com>
2022-10-12 13:29:22 +0700

    Update ScientificCalculator.cpp

--- ScientificCalculator.cpp
+++ ScientificCalculator.cpp
@@ -1,11 +1,20 @@
-#include "ScientificCalculator.h"
+#ifndef SCIENTIFICCALCULATOR_H
+#define SCIENTIFICCALCULATOR_H
 
-ScientificCalculator::ScientificCalculator()
-{
-    //ctor
-}
 
-ScientificCalculator::~ScientificCalculator()
+class ScientificCalculator
 {
-    //dtor
-}
+    public:
+        ScientificCalculator();
+        void Calculation(int choice);
+        void Continuing_Calculation(int choice);
+        void Alternate_Calculation(int choice);
+        void Alternate_Calculation_2(int choice);
+        void Save_History(void);
+        void Print_History(void);
+
+    private:
+        int Option;
+};
+
+#endif // SCIENTIFICCALCULATOR_H
